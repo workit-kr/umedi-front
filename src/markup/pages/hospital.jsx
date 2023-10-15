da4ec56141f3cd314e8c8e3798f107d15b41e39c
김수미 <sumik@emart.com>
2023-10-15 13:38:55 +0900

    구글 지도 추가

--- src/markup/pages/hospital.jsx
+++ src/markup/pages/hospital.jsx
@@ -10,6 +10,7 @@ import { Badge } from 'react-bootstrap';
 import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner';
 import { useNavigate } from "react-router-dom";
+import HospitalModal from '../elements/hospital-modal';
 
 const {
   REACT_APP_UMEDI_URL,
@@ -26,9 +27,11 @@ function Hospital(){
 	const [hospitalList, setHospitalList] = useState([]);
 	const [filteredHospitalList, setFilteredHospitalList] = useState([]);
 	const [selectedLocation, setSelectedLocation] = useState("");
+	const [selectedHospital, setSelectedHospital] = useState("");
 	const [isMatched, setIsMatched] = useState(true);	
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
+	const [openModal, setOpenModal] = useState(false);
 
 	useEffect(() => {
 		fetchHospitalList();
@@ -148,8 +151,14 @@ function Hospital(){
 		}
 	}
 
+	const handleHospitalClicked = (hospital) => {
+		setSelectedHospital(hospital);
+		setOpenModal(true);
+	}
+
 	return(
 		<>
+			<HospitalModal show={openModal} handleClose={() => setOpenModal(false)} hospital={selectedHospital} okButton="Enter Now!" handleOK={null}/>
 			<div className="page-content bg-white">
 				<section className="section-area section-sp1 bg-white">
 					<div className="container">
@@ -236,8 +245,13 @@ function Hospital(){
 														<div className="hospital-wrap">
 															<div className="hospital-row">
 																<div className="hospital-info">
-																	<div className="hospital-info-content">
-																		<span className="title">{hospital.name}</span>
+																	<div className="hospital-info-content" onClick={() => handleHospitalClicked(hospital)}>
+																		<span className="title">{hospital.name}
+																			<svg width="24" height="24" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+																				<path fillRule="evenodd" clipRule="evenodd" d="M15.0001 26.4857L15.9016 25.47C16.9244 24.2986 17.8444 23.1871 18.663 22.13L19.3387 21.2386C22.1601 17.4371 23.5716 14.42 23.5716 12.19C23.5716 7.43 19.7344 3.57143 15.0001 3.57143C10.2659 3.57143 6.42871 7.43 6.42871 12.19C6.42871 14.42 7.84014 17.4371 10.6616 21.2386L11.3373 22.13C12.5051 23.6263 13.7268 25.0782 15.0001 26.4857Z" stroke="black" strokeLinecap="round" strokeLinejoin="round"/>
+																				<path d="M15.0001 15.7143C16.9726 15.7143 18.5716 14.1153 18.5716 12.1429C18.5716 10.1704 16.9726 8.57143 15.0001 8.57143C13.0277 8.57143 11.4287 10.1704 11.4287 12.1429C11.4287 14.1153 13.0277 15.7143 15.0001 15.7143Z" stroke="black" strokeLinecap="round" strokeLinejoin="round"/>
+																			</svg>
+																			</span>
 																		<span className="text-secondary">{hospital?.speciality1_name} {hospital?.speciality2_name ? `| ${hospital?.speciality2_name }` : ''}</span>
 																		<span className="text-secondary">{hospital?.city}</span>
 																	</div>
