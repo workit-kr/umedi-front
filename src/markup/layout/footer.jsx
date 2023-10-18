e708cd867b6c30a752d98bb0a6168b45160157ff
김수미 <sumik@emart.com>
2023-10-18 22:23:28 +1000

    footer 정보 및 로직 추가

--- src/markup/layout/footer.jsx
+++ src/markup/layout/footer.jsx
@@ -15,94 +15,46 @@ import twitter from '../../images/social/twitter.png';
 import instagram from '../../images/social/instagram.png';
 import linkedin from '../../images/social/linkedin.png';
 
+
+import umedi from '../../images/u-medi-white.png';
+
 class aboutSection extends Component{
 	render(){
+	
+		const resetData = () => {
+			sessionStorage.setItem('bookingObject', JSON.stringify({}));
+		}
+
 		return(
 			<>
-
-				<footer className="footer" style={{backgroundImage: "url("+footerBg+")"}}>
+				<footer className="footer">
 					<div className="footer-top">
 						<div className="container">
 							<div className="row">
-								<div className="col-xl-3 col-lg-3 col-md-6">
-									<div className="widget widget_info">
-										<div className="footer-logo">
-											<Link to="/"><img src={Logo} alt=""/></Link>
-										</div>
-										<div className="ft-contact">
-											<p>Lorem ipsum is dolor sit amet, csectetur adipiscing elit, dolore smod tempor incididunt ut labore et.</p>
-											<div className="contact-bx">
-												<div className="icon"><i className="fas fa-phone-alt"></i></div>
-												<div className="contact-number">
-													<span>Contact Us</span>
-													<h4 className="number">+01 123 456 7890</h4>
-												</div>
-											</div>
-										</div>
+								<div className="widget widget_info">
+									<div className='sns-links'>
+										<a type="button" href="https://www.facebook.com/umediwellness/" target="_blank">
+											<i className='fab fa-facebook-f'/>
+										</a>
+										<a href="mailto:info@u-medi.com" target="_blank">
+											<i className='fas fa-envelope'/>
+										</a>
+										<a type="button" href="http://968vp.channel.io/support-bots/51785" target="_blank">
+											<i className='fas fa-comment-dots'/>
+										</a>
 									</div>
-								</div>
-								<div className="col-xl-3 col-lg-3 col-6">
-									<div className="widget footer_widget ml-50">
-										<h3 className="footer-title">Quick Links</h3>
-										<ul>
-											<li><Link to="/about-us"><span>About Us</span></Link></li>
-											<li><Link to="/services"><span>Services</span></Link></li>
-											<li><Link to="/booking"><span>Booking</span></Link></li>
-											<li><Link to="/faq"><span>Faq's</span></Link></li>
-											<li><Link to="/blog-grid"><span>Blogs</span></Link></li>
-											<li><Link to="/team"><span>Out Team</span></Link></li>
-										</ul>
+									<div className="footer-logo">
+										<Link to="/main"><img src={umedi} alt="main page" onClick={resetData}/></Link>
 									</div>
-								</div>
-								<div className="col-xl-3 col-lg-3 col-6">
-									<div className="widget footer_widget">
-										<h3 className="footer-title">Our Service</h3>
-										<ul>
-											<li><Link to="/service-detail"><span>Dental Care</span></Link></li>
-											<li><Link to="/service-detail"><span>Cardiac Clinic</span></Link></li>
-											<li><Link to="/service-detail"><span>Massege Therapy</span></Link></li>
-											<li><Link to="/service-detail"><span>Cardiology</span></Link></li>
-											<li><Link to="/service-detail"><span>Precise Diagnosis</span></Link></li>
-											<li><Link to="/service-detail"><span>Abmbulance Services</span></Link></li>
-										</ul>
-									</div>
-								</div>
-								<div className="col-xl-3 col-lg-3 col-md-6">
-									<div className="widget widget_form">
-										<h3 className="footer-title">Subcribe</h3>
-										<form className="subscribe-form subscription-form mb-30">
-											<div className="ajax-message"></div>
-											<div className="input-group">
-												<input name="email" required="required" className="form-control" placeholder="Email Address" type="email"/>
-											</div>
-											<button name="submit" value="Submit" type="submit" className="btn btn-secondary shadow w-100">Subscribe Now</button>
-										</form>
-										<div className="footer-social-link">
-											<ul>
-												<li><a target="_blank" rel="noreferrer" href="https://www.facebook.com/"><img src={facebook} alt=""/></a></li>
-												<li><a target="_blank" rel="noreferrer" href="https://twitter.com/"><img src={twitter} alt=""/></a></li>
-												<li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/"><img src={instagram} alt=""/></a></li>
-												<li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/"><img src={linkedin} alt=""/></a></li>
-											</ul>
-										</div>
-									</div>
+									<div className="footer-text"><span className='medium bold'>U-MEDI Co., Ltd.</span><span>Business License:  131-86-60359</span></div>
+									<div className="footer-text"><span style={{ display: 'block' }}>Tel. 070-4639-0022 &nbsp;&nbsp;&nbsp; E-mail. info<a href="mailto:wellness@u-medi.com">@u-medi.com</a>&nbsp;&nbsp;&nbsp;Add. 23 Fl. 263, Central-ro, Yeonsu-gu, Incheon</span></div>
+									<br /><br />
+									<div className="footer-text"><span className='medium'>Terms and conditions</span><span className='medium'>Privacy policy</span></div>
+									<div className="footer-text"><span>Copyright ⓒ 2023 U-MEDIㅣYour MEDIcal Platform All rights reserved.</span></div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div className="container">
-						<div className="footer-bottom">
-							<div className="row">
-								<div className="col-12 text-center">
-									<p className="copyright-text">Copyright © 2023 Design & Developed by <a href="https://themeforest.net/user/themetrades" rel="noreferrer" target="_blank" className="text-secondary">ThemeTrades</a></p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<img className="pt-img1 animate-wave" src={ptImg1} alt=""/>
-					<img className="pt-img2 animate1" src={ptImg2} alt=""/>
-					<img className="pt-img3 animate-rotate" src={ptImg3} alt=""/>
-					<img className="pt-img4 animate-wave" src={ptImg4} alt=""/>
 				</footer>
 
 			</>
